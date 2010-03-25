87629a1e4f57ab8a7d9991c37186daba986f406d
Jeroen Jacobs <jacobsjeroen@gmail.com>
2010-03-25 13:54:34 +0100

    Added precision option

--- lib/geo_ip.rb
+++ lib/geo_ip.rb
@@ -1,4 +1,6 @@
-SERVICE_URL = "http://ipinfodb.com/ip_query.php"
+SERVICE_URL = "http://ipinfodb.com/"
+CITY_API    = "ip_query.php"
+COUNTRY_API = "ip_query_country.php"
 IPV4_REGEXP = /\A(?:25[0-5]|(?:2[0-4]|1\d|[1-9])?\d)(?:\.(?:25[0-5]|(?:2[0-4]|1\d|[1-9])?\d)){3}\z/
 
 require 'rubygems'
@@ -9,13 +11,17 @@ require 'net/http'
 class GeoIp
   # <b>DEPRECATED:</b> Please use <tt>geolocation</tt> instead.
   def self.remote_geolocation(ip, timezone=false)
-    warn "[DEPRECATION] `remote_geolocation` is deprecated and will be removed from 0.2. Use `geolocation` instead."
-    geolocation(ip, timezone)
+    warn "DEPRECATION WARNING: `remote_geolocation` is deprecated and will be removed from 0.3. Use `geolocation` instead."
+    geolocation(ip, {:timezone => timezone})
   end
 
-  def self.geolocation(ip, timezone=false)
+  def self.geolocation(ip, options={})
+    @precision = options[:precision] || :city
+    @timezone  = options[:timezone]  || false
     raise "Invalid IP address" unless ip.to_s =~ IPV4_REGEXP
-    uri = SERVICE_URL + "?ip=#{ip}&output=json&timezone=#{timezone}"
+    raise "Invalid precision"  unless [:country, :city].include?(@precision)
+    raise "Invalid timezone"   unless [true, false].include?(@timezone)
+    uri = "#{SERVICE_URL}#{@country ? COUNTRY_API : CITY_API}?ip=#{ip}&output=json&timezone=#{@timezone}"
     url = URI.parse(uri)
     reply = JSON.parse(Net::HTTP.get(url))
     location = convert_keys reply
@@ -24,19 +30,23 @@ class GeoIp
   private
   def self.convert_keys(hash)
     location = {}
-    location[:ip]               = hash["Ip"]
-    location[:status]           = hash["Status"]
-    location[:country_code]     = hash["CountryCode"]
-    location[:country_name]     = hash["CountryName"]
-    location[:region_code]      = hash["RegionCode"]
-    location[:region_name]      = hash["RegionName"]
-    location[:city]             = hash["City"]
-    location[:zip_postal_code]  = hash["ZipPostalCode"]
-    location[:latitude]         = hash["Latitude"]
-    location[:longitude]        = hash["Longitude"]
-    location[:timezone_name]    = hash["TimezoneName"] if hash["TimezoneName"]
-    location[:utc_offset]       = hash["Gmtoffset"].to_i if hash["Gmtoffset"]
-    location[:dst?]             = (hash["Isdst"] ? true : false) unless hash["Isdst"].nil?
+    location[:ip]                 = hash["Ip"]
+    location[:status]             = hash["Status"]
+    location[:country_code]       = hash["CountryCode"]
+    location[:country_name]       = hash["CountryName"]
+    if @precision == :city
+      location[:region_code]      = hash["RegionCode"]
+      location[:region_name]      = hash["RegionName"]
+      location[:city]             = hash["City"]
+      location[:zip_postal_code]  = hash["ZipPostalCode"]
+      location[:latitude]         = hash["Latitude"]
+      location[:longitude]        = hash["Longitude"]
+      if @timezone
+        location[:timezone_name]  = hash["TimezoneName"]
+        location[:utc_offset]     = hash["Gmtoffset"].to_i
+        location[:dst?]           = hash["Isdst"] ? true : false
+      end
+    end
     location
   end
 end
--- spec/geo_ip_spec.rb
+++ spec/geo_ip_spec.rb
@@ -1,45 +1,90 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+IP_GOOGLE_US = '209.85.227.104'
+IP_PRIVATE = '10.0.0.1'
+IP_LOCAL = '127.0.0.1'
 
 describe "GeoIp" do
-  it "should return the correct country for a public ip address" do
-    # 209.85.227.104 = google.be (US)
-    geolocation = GeoIp.geolocation('209.85.227.104')
-    geolocation[:country_code].should == 'US'
-    geolocation[:country_name].should == 'United States'
+  context "city" do
+    it "should return the correct city for a public ip address" do
+      geolocation = GeoIp.geolocation(IP_GOOGLE_US)
+      geolocation[:country_code].should == 'US'
+      geolocation[:country_name].should == 'United States'
+      geolocation[:city].should         == 'Mountain View'
+    end
+
+    it "should return the correct city for a private ip address" do
+      geolocation = GeoIp.geolocation(IP_PRIVATE)
+      geolocation[:country_code].should == 'RD'
+      geolocation[:country_name].should == 'Reserved'
+      geolocation[:city].should         be_empty
+    end
+
+    it "should return the correct city for localhost ip address" do
+      geolocation = GeoIp.geolocation(IP_LOCAL)
+      geolocation[:country_code].should == 'RD'
+      geolocation[:country_name].should == 'Reserved'
+      geolocation[:city].should         be_empty
+    end
+
+    it "should return the correct city for a public ip address when explicitly requiring it" do
+      geolocation = GeoIp.geolocation(IP_GOOGLE_US, {:precision => :city})
+      geolocation[:country_code].should == 'US'
+      geolocation[:country_name].should == 'United States'
+      geolocation[:city].should         == 'Mountain View'
+    end
   end
 
-  it "should return the correct country for a private ip address" do
-    geolocation = GeoIp.geolocation('10.0.0.1')
-    geolocation[:country_code].should == 'RD'
-    geolocation[:country_name].should == 'Reserved'
-  end
+  context "country" do
+    it "should return the correct country for a public ip address" do
+      geolocation = GeoIp.geolocation(IP_GOOGLE_US, {:precision => :country})
+      geolocation[:country_code].should == 'US'
+      geolocation[:country_name].should == 'United States'
+    end
 
-  it "should return the correct country for localhost ip address" do
-    geolocation = GeoIp.geolocation('127.0.0.1')
-    geolocation[:country_code].should == 'RD'
-    geolocation[:country_name].should == 'Reserved'
+    it "should return the correct country for a private ip address" do
+      geolocation = GeoIp.geolocation(IP_PRIVATE, {:precision => :country})
+      geolocation[:country_code].should == 'RD'
+      geolocation[:country_name].should == 'Reserved'
+    end
+
+    it "should return the correct country for localhost ip address" do
+      geolocation = GeoIp.geolocation(IP_LOCAL, {:precision => :country})
+      geolocation[:country_code].should == 'RD'
+      geolocation[:country_name].should == 'Reserved'
+    end
+
+    it "should not return the city for a public ip address" do
+      geolocation = GeoIp.geolocation(IP_GOOGLE_US, {:precision => :country})
+      geolocation[:country_code].should == 'US'
+      geolocation[:country_name].should == 'United States'
+      geolocation[:city].should         be_nil
+    end
   end
 
   context "timezone" do
     it "should return the correct timezone information for a public ip address" do
-      # 209.85.227.104 = google.be (US)
-      geolocation = GeoIp.geolocation('209.85.227.104', true)
+      geolocation = GeoIp.geolocation(IP_GOOGLE_US, {:timezone => true})
       geolocation[:timezone_name].should == 'America/Los_Angeles'
       geolocation[:utc_offset].should    == -25200
       geolocation[:dst?].should_not      be_nil # true if dst?, false if not dst?
     end
 
     it "should not return the timezone information when explicitly not requesting it" do
-      # 209.85.227.104 = google.be (US)
-      geolocation = GeoIp.geolocation('209.85.227.104', false)
+      geolocation = GeoIp.geolocation(IP_GOOGLE_US, {:timezone => false})
       geolocation[:timezone_name].should be_nil
       geolocation[:utc_offset].should    be_nil
       geolocation[:dst?].should          be_nil
     end
 
     it "should not return the timezone information when not requesting it" do
-      # 209.85.227.104 = google.be (US)
-      geolocation = GeoIp.geolocation('209.85.227.104')
+      geolocation = GeoIp.geolocation(IP_GOOGLE_US)
+      geolocation[:timezone_name].should be_nil
+      geolocation[:utc_offset].should    be_nil
+      geolocation[:dst?].should          be_nil
+    end
+
+    it "should not return the timezone information when country precision is selected" do
+      geolocation = GeoIp.geolocation(IP_GOOGLE_US, {:precision => :country, :timezone => true})
       geolocation[:timezone_name].should be_nil
       geolocation[:utc_offset].should    be_nil
       geolocation[:dst?].should          be_nil
@@ -48,8 +93,7 @@ describe "GeoIp" do
 
   context "deprecated" do
     it "should return the correct country for a public ip address" do
-      # 209.85.227.104 = google.be (US)
-      geolocation = GeoIp.geolocation('209.85.227.104')
+      geolocation = GeoIp.remote_geolocation(IP_GOOGLE_US)
       geolocation[:country_code].should == 'US'
       geolocation[:country_name].should == 'United States'
     end
