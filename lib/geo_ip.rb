eef9540d2fc8b683c2880fffae3a6eabe185cdb7
Jeroen Jacobs <jacobsjeroen@gmail.com>
2010-03-25 13:12:43 +0100

    Added optional timezone information

--- lib/geo_ip.rb
+++ lib/geo_ip.rb
@@ -8,14 +8,14 @@ require 'net/http'
 
 class GeoIp
   # <b>DEPRECATED:</b> Please use <tt>geolocation</tt> instead.
-  def self.remote_geolocation(ip)
+  def self.remote_geolocation(ip, timezone=false)
     warn "[DEPRECATION] `remote_geolocation` is deprecated and will be removed from 0.2. Use `geolocation` instead."
-    geolocation(ip)
+    geolocation(ip, timezone)
   end
 
-  def self.geolocation(ip)
+  def self.geolocation(ip, timezone=false)
     raise "Invalid IP address" unless ip.to_s =~ IPV4_REGEXP
-    uri = SERVICE_URL + "?ip=#{ip}&output=json&timezone=false"
+    uri = SERVICE_URL + "?ip=#{ip}&output=json&timezone=#{timezone}"
     url = URI.parse(uri)
     reply = JSON.parse(Net::HTTP.get(url))
     location = convert_keys reply
@@ -34,6 +34,9 @@ class GeoIp
     location[:zip_postal_code]  = hash["ZipPostalCode"]
     location[:latitude]         = hash["Latitude"]
     location[:longitude]        = hash["Longitude"]
+    location[:timezone_name]    = hash["TimezoneName"] if hash["TimezoneName"]
+    location[:utc_offset]       = hash["Gmtoffset"].to_i if hash["Gmtoffset"]
+    location[:dst?]             = (hash["Isdst"] ? true : false) unless hash["Isdst"].nil?
     location
   end
 end
--- spec/geo_ip_spec.rb
+++ spec/geo_ip_spec.rb
@@ -20,6 +20,32 @@ describe "GeoIp" do
     geolocation[:country_name].should == 'Reserved'
   end
 
+  context "timezone" do
+    it "should return the correct timezone information for a public ip address" do
+      # 209.85.227.104 = google.be (US)
+      geolocation = GeoIp.geolocation('209.85.227.104', true)
+      geolocation[:timezone_name].should == 'America/Los_Angeles'
+      geolocation[:utc_offset].should    == -25200
+      geolocation[:dst?].should_not      be_nil # true if dst?, false if not dst?
+    end
+
+    it "should not return the timezone information when explicitly not requesting it" do
+      # 209.85.227.104 = google.be (US)
+      geolocation = GeoIp.geolocation('209.85.227.104', false)
+      geolocation[:timezone_name].should be_nil
+      geolocation[:utc_offset].should    be_nil
+      geolocation[:dst?].should          be_nil
+    end
+
+    it "should not return the timezone information when not requesting it" do
+      # 209.85.227.104 = google.be (US)
+      geolocation = GeoIp.geolocation('209.85.227.104')
+      geolocation[:timezone_name].should be_nil
+      geolocation[:utc_offset].should    be_nil
+      geolocation[:dst?].should          be_nil
+    end
+  end
+
   context "deprecated" do
     it "should return the correct country for a public ip address" do
       # 209.85.227.104 = google.be (US)
