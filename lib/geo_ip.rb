59ba557cd6d5ef3c2812577cf821394222a41084
Jeroen Jacobs <info@jeroenj.be>
2011-05-20 23:02:16 +0200

    Refactors internal structure of the code

--- lib/geo_ip.rb
+++ lib/geo_ip.rb
@@ -8,65 +8,66 @@ require 'rest-client'
 
 class GeoIp
   @@api_key = nil
-
-  def self.api_key
-    @@api_key
-  end
-
-  def self.api_key=(api_key)
-    @@api_key = api_key
-  end
-
   @@timeout = 1
 
-  def self.timeout
-    @@timeout
-  end
-
-  def self.timeout= timeout
-    @@timeout = timeout
-  end
-
-  # Retreive the remote location of a given ip address.
-  #
-  # It takes two optional arguments:
-  # * +preceision+: can either be +:city+ (default) or +:country+
-  # * +timezone+: can either be +false+ (default) or +true+
-  #
-  # ==== Example:
-  #   GeoIp.geolocation('209.85.227.104', {:precision => :city, :timezone => true})
-  def self.geolocation(ip, options={})
-    @precision = options[:precision] || :city
-    @timezone  = options[:timezone]  || false
-    raise "API key must be set first: GeoIp.api_key = 'YOURKEY'" if self.api_key.nil?
-    raise "Invalid IP address" unless ip.to_s =~ IPV4_REGEXP
-    raise "Invalid precision"  unless [:country, :city].include?(@precision)
-    raise "Invalid timezone"   unless [true, false].include?(@timezone)
-    url = "#{SERVICE_URL}#{@country ? COUNTRY_API : CITY_API}?key=#{self.api_key}&ip=#{ip}&output=json&timezone=#{@timezone}"
-    reply = JSON.parse RestClient::Request.execute(:method => :get, :url => url, :timeout => self.timeout)
-    location = convert_keys reply
-  end
-
-  private
-  def self.convert_keys(hash)
-    location = {}
-    location[:ip]                 = hash["Ip"]
-    location[:status]             = hash["Status"]
-    location[:country_code]       = hash["CountryCode"]
-    location[:country_name]       = hash["CountryName"]
-    if @precision == :city
-      location[:region_code]      = hash["RegionCode"]
-      location[:region_name]      = hash["RegionName"]
-      location[:city]             = hash["City"]
-      location[:zip_postal_code]  = hash["ZipPostalCode"]
-      location[:latitude]         = hash["Latitude"]
-      location[:longitude]        = hash["Longitude"]
-      if @timezone
-        location[:timezone_name]  = hash["TimezoneName"]
-        location[:utc_offset]     = hash["Gmtoffset"].to_i
-        location[:dst?]           = hash["Isdst"] ? true : false
-      end
+  class << self
+    def api_key
+      @@api_key
     end
-    location
+
+    def api_key= api_key
+      @@api_key = api_key
+    end
+
+    def timeout
+      @@timeout
+    end
+
+    def timeout= timeout
+      @@timeout = timeout
+    end
+
+    # Retreive the remote location of a given ip address.
+    #
+    # It takes two optional arguments:
+    # * +preceision+: can either be +:city+ (default) or +:country+
+    # * +timezone+: can either be +false+ (default) or +true+
+    #
+    # ==== Example:
+    #   GeoIp.geolocation('209.85.227.104', {:precision => :city, :timezone => true})
+    def geolocation ip, options={}
+      @precision = options[:precision] || :city
+      @timezone  = options[:timezone]  || false
+      raise "API key must be set first: GeoIp.api_key = 'YOURKEY'" if self.api_key.nil?
+      raise "Invalid IP address" unless ip.to_s =~ IPV4_REGEXP
+      raise "Invalid precision"  unless [:country, :city].include?(@precision)
+      raise "Invalid timezone"   unless [true, false].include?(@timezone)
+      url = "#{SERVICE_URL}#{@country ? COUNTRY_API : CITY_API}?key=#{self.api_key}&ip=#{ip}&output=json&timezone=#{@timezone}"
+      reply = JSON.parse RestClient::Request.execute(:method => :get, :url => url, :timeout => self.timeout)
+      location = convert_keys reply
+    end
+
+    private
+    def convert_keys hash
+      location = {}
+      location[:ip]                 = hash["Ip"]
+      location[:status]             = hash["Status"]
+      location[:country_code]       = hash["CountryCode"]
+      location[:country_name]       = hash["CountryName"]
+      if @precision == :city
+        location[:region_code]      = hash["RegionCode"]
+        location[:region_name]      = hash["RegionName"]
+        location[:city]             = hash["City"]
+        location[:zip_postal_code]  = hash["ZipPostalCode"]
+        location[:latitude]         = hash["Latitude"]
+        location[:longitude]        = hash["Longitude"]
+        if @timezone
+          location[:timezone_name]  = hash["TimezoneName"]
+          location[:utc_offset]     = hash["Gmtoffset"].to_i
+          location[:dst?]           = hash["Isdst"] ? true : false
+        end
+      end
+      location
+    end
   end
 end
